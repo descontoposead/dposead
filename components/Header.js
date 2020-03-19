dbe346690cd636851d13fc75e6974ad3bdaf5b0d
tavoli <gustavojnt@gmail.com>
2020-03-19 20:12:56 -0300

    removendo console.log

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,19 @@
-const Header = props => {
-  console.log(props);
-  return (
-    <>
-      {props.onlyToolbar ? (
+const Header = props => (
+  <>
+    {props.onlyToolbar ? (
+      <div className="toolbar">
+        <img
+          className="logotipo"
+          src="/logotipo.webp"
+          alt="Logo da Desconto POSEAD"
+        />
+        <div className="cta">
+          <button className="btn btn-outline">Falar com consultor</button>
+          <button className="btn btn-write">Matricule-se</button>
+        </div>
+      </div>
+    ) : (
+      <header>
         <div className="toolbar">
           <img
             className="logotipo"
@@ -14,32 +25,18 @@ const Header = props => {
             <button className="btn btn-write">Matricule-se</button>
           </div>
         </div>
-      ) : (
-        <header>
-          <div className="toolbar">
-            <img
-              className="logotipo"
-              src="/logotipo.webp"
-              alt="Logo da Desconto POSEAD"
-            />
-            <div className="cta">
-              <button className="btn btn-outline">Falar com consultor</button>
-              <button className="btn btn-write">Matricule-se</button>
-            </div>
-          </div>
 
-          <div style={{ zIndex: 1 }} className="cta">
-            <h1>Você não precisa pagar caro para ter educação de qualidade</h1>
-            <hr />
-            <div>
-              <span>Estude agora com descontos imperdiveis</span>
-              <button className="btn btn-red">Falar com um consultor</button>
-            </div>
+        <div style={{ zIndex: 1 }} className="cta">
+          <h1>Você não precisa pagar caro para ter educação de qualidade</h1>
+          <hr />
+          <div>
+            <span>Estude agora com descontos imperdiveis</span>
+            <button className="btn btn-red">Falar com um consultor</button>
           </div>
-        </header>
-      )}
-    </>
-  );
-};
+        </div>
+      </header>
+    )}
+  </>
+);
 
 export default Header;
