054e5824813d2ddc535c04785fa1fe785bbb4eb1
tavoli <gustavojnt@gmail.com>
2020-04-12 23:33:28 -0300

    Ao reliazar transação com egoi devemos tratar de maneira exclusiva para ter mais especialidade no tratamento dos erros

--- src/pages/api/leads.js
+++ src/pages/api/leads.js
@@ -3,21 +3,18 @@ import joi from '@hapi/joi'
 import fetch from 'node-fetch'
 import absoluteUrl from 'next-absolute-url'
 
-const secret = process.env.DPOS_FAUNADB_SECRET_KEY
 const q = faunadb.query
-const client = new faunadb.Client({ secret })
+const client = new faunadb.Client({
+  secret: process.env.DPOS_FAUNADB_SECRET_KEY,
+})
 
 export default async (req, res) => {
-  //amp pages send the param
-  if (req.query.hasOwnProperty) {
-    delete req.query.__amp_source_origin
-  }
-
   const schema = joi
     .object({
       name: joi.string().required(),
       email: joi.string().required(),
       whatsapp: joi.string().required(),
+      __amp_source_origin: joi.string(),
     })
     .required()
     .label('lead')
@@ -28,11 +25,12 @@ export default async (req, res) => {
     return res.status(400).json(error)
   }
 
+  //save lead on egoi platform
   try {
-    //save lead on egoi platform
     const fullName = value.name.split(' ')
     const haveLastName = fullName.length > 1
-    fetch('https://api.egoiapp.com/lists/1/contacts', {
+
+    const res = await fetch('https://api.egoiapp.com/lists/1/contacts', {
       headers: {
         'Content-Type': 'application/json',
         Apikey: process.env.DPOS_EGOI_API_KEY,
@@ -43,30 +41,34 @@ export default async (req, res) => {
           email: value.email,
           first_name: fullName[0],
           last_name: haveLastName ? fullName.pop() : '',
-          cellphone: '55-' + value.whatsapp, //all brazil code by default
+          cellphone: '55-' + value.whatsapp, //brazil code by default
         },
       }),
     })
-      .then((res) => res.json())
-      .then(({ contact_id }) =>
-        fetch('https://api.egoiapp.com/lists/1/contacts/actions/attach-tag', {
-          headers: {
-            'Content-Type': 'application/json',
-            Apikey: process.env.DPOS_EGOI_API_KEY,
-          },
-          method: 'post',
-          body: JSON.stringify({
-            tag_id: 1,
-            contacts: [contact_id],
-          }),
-        })
-      )
-      .catch(console.log)
 
-    //save lead on dpos datasource
+    const { contact_id } = res.json()
+
+    fetch('https://api.egoiapp.com/lists/1/contacts/actions/attach-tag', {
+      headers: {
+        'Content-Type': 'application/json',
+        Apikey: process.env.DPOS_EGOI_API_KEY,
+      },
+      method: 'post',
+      body: JSON.stringify({
+        tag_id: 1,
+        contacts: [contact_id],
+      }),
+    })
+  } catch (err) {
+    console.error(err)
+  }
+
+  //save lead on dpos datasource
+  try {
     const dbs = await client.query(
       q.Create(q.Collection('leads'), { data: value })
     )
+    console.log(absoluteUrl(req))
     res.setHeader('AMP-Redirect-To', `${absoluteUrl(req).origin}/obrigado`)
     res.status(201).json(dbs.data)
   } catch (e) {
