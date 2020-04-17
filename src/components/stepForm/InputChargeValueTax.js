56de82dcd646f1dc334861d1b18b4f060a7f8ef3
tavoli <gustavojnt@gmail.com>
2020-04-17 00:32:04 -0300

    valor da tava de matricula

--- src/components/stepForm/InputChargeValueTax.js
+++ src/components/stepForm/InputChargeValueTax.js
@@ -23,7 +23,7 @@ const InputChargeValueTax = () => {
       name: 'chargeValueTax',
       value: {
         instalment: 1,
-        value: 5,
+        value: 500,
         currency: 5,
       },
     })
@@ -34,7 +34,7 @@ const InputChargeValueTax = () => {
       //reset
       const initialChargeTaxValue = {
         instalment: 1,
-        value: 5,
+        value: 500,
         currency: 5,
       }
       setChargeValueTax(initialChargeTaxValue)
